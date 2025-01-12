c62afcfca0a5c50af2fcf5492ceb18c8a54b167f
clsid52 <clsid52@gmail.com>
2025-01-12 00:45:49 +0000

    Update install-wp-plugins.sh
    v4

--- scripts/install-wp-plugins.sh
+++ scripts/install-wp-plugins.sh
@@ -1,1 +1,12 @@
-TO ADD a bash script to download blabla
+#!/bin/bash
+set -euo pipefail
+
+echo "Adding permissions"
+chown -R root:www-data /var/www/html/wp-content &amp;
+find /var/www/html/wp-content -type d -exec chmod 0755 {} \; &amp;
+find /var/www/html/wp-content -type f -exec chmod 644 {} \; &amp;
+echo "Permissions added"
+
+exec install-wp-plugins.sh "$@"
+
+
