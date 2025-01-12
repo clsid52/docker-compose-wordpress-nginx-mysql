bca1f28307b8effbc2c6c236cdac7986cd720840
clsid52 <clsid52@gmail.com>
2025-01-12 00:56:51 +0000

    Update install-wp-plugins_and_themes.sh
    v3

--- scripts/install-wp-plugins_and_themes.sh
+++ scripts/install-wp-plugins_and_themes.sh
@@ -6,5 +6,13 @@ chown -R root:www-data /var/www/html/wp-content &amp;
 find /var/www/html/wp-content -type d -exec chmod 0755 {} \; &amp;
 find /var/www/html/wp-content -type f -exec chmod 644 {} \; &amp;
 echo "Permissions added"
-
+echo "...."
+echo "...."
+echo "...."
+echo "...."
+echo "Installing Plugins"
+curl 1
+curl 2
+curl 3
+curl 4
 exec install-wp-plugins.sh "$@"
